bd8be09ac6a0a9c982dbee08a3a181eb32596a65
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-10-26 07:57:33 +0100

    add firebase to main.js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import App from './App'
 import router from './router'
 import store from '@/store'
 import AppDate from '@/components/AppDate'
-import {firebase} from '@/config/firebase'
+import firebase from 'firebase'
+import {config} from '@/config/firebase'
 
 Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
 // Vue.config.silent = true
+
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
