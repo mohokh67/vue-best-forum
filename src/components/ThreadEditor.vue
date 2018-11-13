fe75648aab4525c336c1f2c9e974dd36ba94242a
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-11-13 16:52:14 +0000

    [Feature] validation for create/update thread

--- src/components/ThreadEditor.vue
+++ src/components/ThreadEditor.vue
@@ -4,22 +4,32 @@
         <label for="threadTitle">Title:</label>
         <input
           v-model="form.title"
+          @blur="$v.form.title.$touch()"
           type="text"
           class="form-input"
           id="threadTitle"
           name="title">
+          <template v-if="$v.form.title.$error">
+            <span v-if="!$v.form.title.required" class="form-error">It is required</span>
+            <span v-else-if="!$v.form.title.minLength" class="form-error">Minimum 7 characters</span>
+          </template>
       </div>
 
       <div class="form-group">
         <label for="threadContent">Content:</label>
         <textarea
           v-model="form.text"
+          @blur="$v.form.text.$touch()"
           name="content"
           id="threadContent"
           cols="140"
           rows="8"
           class="form-input">
         </textarea>
+        <template v-if="$v.form.text.$error">
+            <span v-if="!$v.form.text.required" class="form-error">It is required</span>
+            <span v-else-if="!$v.form.text.minLength" class="form-error">Minimum 40 characters</span>
+          </template>
       </div>
 
       <div class="btn-group">
@@ -29,6 +39,8 @@
     </form>
 </template>
 <script>
+  import {required, minLength, email, url} from 'vuelidate/lib/validators'
+
   export default {
     props: {
       title: {
@@ -50,6 +62,19 @@
       }
     },
 
+    validations: {
+      form: {
+        title: {
+          required,
+          minLength: minLength(7)
+        },
+        text: {
+          required,
+          minLength: minLength(40)
+        }
+      }
+    },
+
     computed: {
       isUpdating () {
         return !!this.title
@@ -58,6 +83,12 @@
 
     methods: {
       save () {
+        this.$v.form.$touch()
+        if (this.$v.form.$invalid) {
+          console.log('Thread editor not submitted. Error in form validation')
+          return
+        }
+
         this.$emit('save', {title: this.form.title, text: this.form.text})
       },
 
